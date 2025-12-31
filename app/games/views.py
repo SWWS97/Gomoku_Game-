fa8696e81f66b7216a67ec8bded3e0a68bf7fa98
SWWS97 <parkmon26@gmail.com>
2026-01-01 00:52:26 +0900

    🐛 fix : import 위치 수정

--- app/games/views.py
+++ app/games/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 
-from accounts.models import UserProfile
+from app.accounts.models import UserProfile
 
 from .models import BOARD_SIZE, Game, GameHistory, Move
 
