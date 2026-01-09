cc9dd0b2a8ce338785fc8c679eda8b0f86ea11dd
SWWS97 <parkmon26@gmail.com>
2026-01-09 18:13:01 +0900

    ✨ feat : 사용자 전적 프로필 툴팁, 상세 프로필 추가

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ProfileEditView, SignUpView
+from .views import ProfileEditView, SignUpView, user_profile
 
 app_name = "accounts"
 urlpatterns = [
     path("signup/", SignUpView, name="signup"),  # 함수형 뷰이므로 as_view() 제거
     path("profile/edit/", ProfileEditView, name="profile_edit"),
+    path("profile/<str:username>/", user_profile, name="profile"),
 ]
